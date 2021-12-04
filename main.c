577ab0ffef52e12a851ec1f4e4313772399c1535
somia <somia.floret@outlook.com>
2021-12-04 17:04:01 +0100

    marche pas

--- main.c
+++ main.c
@@ -1,28 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/ipc.h> 
-
-//headers
 #include "headers/semaphore.h"
 
-#define nbr_cargo 10
-#define nbr_wagon 10
-#define nbr_camion 10
+#define CAPACITE_PENICHE 10
+#define CAPACITE_TRAIN 10
+#define NB_CAMION 10
 
 
-typedef enum {NORD, SUD, OUEST, EST, ETRANGER} Destination;
+typedef enum {NORD, SUD, OUEST, EST} Destination;
 //Definition de la struture Conteneur
 typedef struct Container {
     pid_t id;
     Destination destination;
-} Container;
+} Container; 
 
 int nombre_aleatoire(int min, int max) {
   return(min + (rand() % (max - min)));
 }
+/*//ton code Samy
+Container * remplir_transport(Container *transport, Destination destination, int nb_container) {
+
+    Container container_peniche[CAPACITE_PENICHE];
+
+    for(int i=0; i < nb_container; i++) {
+        Container cont;
+        cont = creer_container(destination);
+        container_peniche[i].destination = cont.destination;
+        container_peniche[i].id = cont.id;
+    }
+
+    return container_peniche;
+}*/
+void remplir_transport(Container transport[], Destination dest, int nb_container) {
+    
+    for (int i=0; i < nb_container; i++) {
+        transport[i] = creer_container(dest);
+    }
+
+
+}
+
 
 int creer_portique(Container container) 
 {
@@ -57,13 +72,26 @@ int creer_portique(Container container)
     }
 }
 
-int creer_peniche(int portique)
+int creer_peniche()
 {
     int pid = fork();
     if(pid != 0)
         return pid;
 
-    
+    Destination destination = nombre_aleatoire(0, 3);
+    int nb_container = nombre_aleatoire(0, CAPACITE_PENICHE);
+
+    Container container_peniche[CAPACITE_PENICHE];
+    remplir_transport(container_peniche, destination, nb_container);
+
+    //ton code Samy
+   /* Destination destination = nombre_aleatoire(0, 3);
+    int nb_container = nombre_aleatoire(0, CAPACITE_PENICHE);
+
+    Container container_peniche[CAPACITE_PENICHE];
+    remplir_transport(container_peniche, destination, nb_container);
+    */
+
 
     exit(0);
 }
@@ -74,6 +102,8 @@ int creer_camion(int portique)
     if(pid != 0)
         return pid;
 
+    /*Destination destination = nombre_aleatoire(0, 3);
+    int nb_container = nombre_aleatoire(0, 1);*/
 
     exit(0);
 }
@@ -88,15 +118,16 @@ int creer_train(Destination destination) //2 trains unidirectionnels
     exit(0);
 }
 
-Container creer_Container()
+Container creer_container(Destination destination)
 {
     pid_t pid = fork();
     if(pid != 0){
-        Container container;
-
-        container.destination = nombre_aleatoire(0, 4);
-        container.id = pid;
-        return container;
+        Container cont;
+        do{
+            cont.destination = nombre_aleatoire(0, 3);
+        } while(cont.destination != destination);        
+        cont.id = pid;
+        return cont;
     }
 }
 
@@ -112,22 +143,24 @@ int main(int argc, char *argv[])
     int portique, peniche, camion; //train
 
     srand(time(NULL));
-    
+
+    creer_peniche();
+
     for(int i = 0; i < 2; i++) {
         sleep(nombre_aleatoire(0, 5));
-        Container container = creer_Container();
+        /*Container container = creer_container();
         portique = creer_portique(container);
-        peniche = creer_peniche(portique);
+        peniche = creer_peniche(portique);*/
     }
 
-    for(int i = 0; i < nbr_camion; i++) {
+    for(int i = 0; i < NB_CAMION; i++) {
         sleep(nombre_aleatoire(0, 5));
-        camion = creer_camion(portique);
+        //camion = creer_camion(portique);
     }
 
     for(int i = 0; i < n; i++) {
         sleep(nombre_aleatoire(0, 5));
-        creer_Container();
+        //creer_container();
     }
   
 
